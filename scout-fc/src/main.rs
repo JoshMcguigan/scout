e1993224c059c679cbd4d71e49c8900170cfe6e5
Josh Mcguigan <joshmcg88@gmail.com>
2023-02-03 22:43:54 -0800

    setup i2c on fc

--- scout-fc/src/main.rs
+++ scout-fc/src/main.rs
@@ -8,12 +8,14 @@ use embassy_embedded_hal::shared_bus::asynch::spi::SpiDevice;
 use embassy_executor::Spawner;
 use embassy_stm32::{
     gpio::{Level, Output, Speed},
+    i2c::{self, I2c},
+    interrupt::{self, Interrupt},
     peripherals::{DMA2_CH0, DMA2_CH3, SPI1},
     spi::{self, Spi},
-    time::mhz,
+    time::{mhz, Hertz},
 };
 use embassy_sync::{blocking_mutex::raw::ThreadModeRawMutex, mutex::Mutex};
-use embassy_time::{Delay, Duration, Timer};
+use embassy_time::{Delay, Duration, Instant, Timer};
 use static_cell::StaticCell;
 
 use panic_probe as _;
@@ -27,7 +29,36 @@ static SPI_BUS: StaticCell<Mutex<ThreadModeRawMutex, SpiBus1>> = StaticCell::new
 async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
 
+    // PB8 is labeled D15 on the NUCLEO-F446RE
+    // PB9 is labeled D14 on the NUCLEO-F446RE
+    // See UM1724 Table 19
+    let scl = p.PB8;
+    let sda = p.PB9;
+    let i2c_config = {
+        let mut c = i2c::Config::default();
+        c.sda_pullup = true;
+        c.scl_pullup = true;
+        c
+    };
+    let mut tail_rotor_esc_i2c = I2c::new(
+        p.I2C1,
+        scl,
+        sda,
+        // Safety: This interrupt isn't used anywhere else.
+        //
+        // TODO why do I have to steal this? Should it be part of the Peripheral singleton?
+        unsafe { interrupt::I2C1_EV::steal() },
+        p.DMA1_CH0,
+        p.DMA1_CH1,
+        Hertz::khz(2),
+        i2c_config,
+    );
+
     let spi_bus_1 = {
+        // PA5 is labeled D13 on the NUCLEO-F446RE
+        // PA6 is labeled D12 on the NUCLEO-F446RE
+        // PA7 is labeled D11 on the NUCLEO-F446RE
+        // See UM1724 Table 19
         let sck = p.PA5;
         let miso = p.PA6;
         let mosi = p.PA7;
@@ -43,12 +74,15 @@ async fn main(_spawner: Spawner) {
         );
         SPI_BUS.init(Mutex::<ThreadModeRawMutex, _>::new(spi))
     };
-    let csn = Output::new(p.PB6, Level::High, Speed::High);
+    // PA8 is labeled D7 on the NUCLEO-F446RE
+    // See UM1724 Table 19
+    let csn = Output::new(p.PA8, Level::High, Speed::High);
+
     let spi_dev_1 = SpiDevice::new(spi_bus_1, csn);
 
-    // PC7 is labeled D9 on the NUCLEO-F446RE
+    // PA9 is labeled D8 on the NUCLEO-F446RE
     // See UM1724 Table 19
-    let ce = Output::new(p.PC7, Level::Low, Speed::High);
+    let ce = Output::new(p.PA9, Level::Low, Speed::High);
 
     let mut radio = unwrap!(SymaX5C::new(spi_dev_1, ce, Delay).await);
 
@@ -56,6 +90,17 @@ async fn main(_spawner: Spawner) {
         match radio.read().await {
             Ok(Some(radio_data)) => {
                 println!("{:?}", radio_data);
+                let i2c_write_start = Instant::now();
+                let i2c_write_timeout = Duration::from_millis(1000);
+                let tail_rotor_write_result =
+                    tail_rotor_esc_i2c.blocking_write_timeout(47, &[234], || {
+                        if i2c_write_start.elapsed() > i2c_write_timeout {
+                            Err(i2c::Error::Timeout)
+                        } else {
+                            Ok(())
+                        }
+                    });
+                println!("{:?}", tail_rotor_write_result);
             }
             Ok(None) => {
                 // No new radio packet available
